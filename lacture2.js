9067a277d630e35d2ed30cbe68eecea5f699da96
masudtek <masud08eee@gmail.com>
2018-12-09 13:10:08 -0500

    finish our first lacture

--- lacture2.js
+++ lacture2.js
@@ -11,19 +11,66 @@
     }
 
  //1. A function can store in a variable 
- var hello = sayName;
- // var fun = sayName("Masud");
- // console.log(hello);
- // console.log(fun);
- // var result = hello("masud");
- // result;
- console.log(hello("masud"));
- var anotherHello = hello;
- console.log(anotherHello("masud"));
+
+    var hello = sayName;
+// var fun = sayName("Masud");
+// console.log(hello);
+// console.log(fun);
+// var result = hello("masud");
+// result;
+    console.log(hello("masud"));
+    var anotherHello = hello;
+    console.log(anotherHello("masud"));
  
  // 2. A Function can be stored in an Array
+
+ var arr = [1,2,3,anotherHello("masud")];
+ arr.push(sayName("khan"));
+ console.log(arr);
+
+ 
+
+
  // 3. A function can stored in an Object field or property
+
+ var person = {
+     name : "Md Masud",
+     funcName: hello,
+     print: function(){
+         console.log("hi");   
+     }
+ }
+ console.log(person.funcName("masud"))
  // 4. We can create function as needed 
- // 5. we can pass function as an argument
+
+ var sum = 10 + (function(){return 20})();
+ sum;
+
+ // 5. we can pass function as an arguments
+
+ function wow(name,fun){
+     return fun(name);
+ }
+ var result = wow("Md Masud kaisar khan",sayName);
+ console.log(result);
+ 
+
  // 6. We can return funcion from another function
+
+ function base(b){
+     return function(n){
+         var result =1;
+         for (var i= 0; i<b; i++ ){
+            result *= n;
+         }
+         return result;
+     }
+ }
+//  var power = base(2);
+//  var result = power(10);
+//  result;
+var result = base(3)(5);
+result;
+
+
 
