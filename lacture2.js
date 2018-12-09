4c8e641c26c19c00fff34f7c33c005d6178eab63
masudtek <masud08eee@gmail.com>
2018-12-09 00:08:41 -0500

    proved our first condition

--- lacture2.js
+++ lacture2.js
@@ -9,17 +9,18 @@
     function sayName(name){
         return "Hello, " + name;
     }
-    var hello = sayName;
-    // var fun = sayName("Masud");
-    // console.log(hello);
-    // console.log(fun);
-    var result = hello("masud");
-    result;
-    
-
-
 
  //1. A function can store in a variable 
+ var hello = sayName;
+ // var fun = sayName("Masud");
+ // console.log(hello);
+ // console.log(fun);
+ // var result = hello("masud");
+ // result;
+ console.log(hello("masud"));
+ var anotherHello = hello;
+ console.log(anotherHello("masud"));
+ 
  // 2. A Function can be stored in an Array
  // 3. A function can stored in an Object field or property
  // 4. We can create function as needed 
