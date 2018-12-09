af10d10921946e8ac77f5d8e2c060f5120212239
masudtek <masud08eee@gmail.com>
2018-12-08 21:19:20 -0500

    start my new functional javaScript

---
+++ lacture2.js
@@ -0,0 +1,21 @@
+/*******
+ * Md Masud Kaisar Khan
+ * Functional JavaScript
+ * First Class Function 
+ * Lacture- 2
+ *  
+  *******/
+ // Todays Topic
+    function sayName(name){
+        return "Hello" + name;
+    }
+    sayName("Masud")
+
+
+ //1. A function can store in a variable 
+ // 2. A Function can be stored in an Array
+ // 3. A function can stored in an Object field or property
+ // 4. We can create function as needed 
+ // 5. we can pass function as an argument
+ // 6. We can return funcion from another function
+
