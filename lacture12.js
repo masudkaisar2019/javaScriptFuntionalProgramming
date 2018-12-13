d7bfb61ac52b154a754f661011d81cae9fe53b7b
masudtek <masud08eee@gmail.com>
2018-12-12 21:12:20 -0500

    we made our map function

---
+++ lacture12.js
@@ -0,0 +1,10 @@
+// learn javaScript map function 
+
+
+var arr = [2,3,4,5,6,8,9];
+
+var mapedArr = [];
+for(var i= o; i<arr.length; i++){
+    mapedArr.push(arr[i]+2);
+}
+console.log('mapped array is '+ mapedArr);
